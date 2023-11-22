d62bd990c917cdcfab799124bdb9e93789233cdd
Imam Hosen <imamhosen9426224@gmail.com>
2023-11-23 01:04:45 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+let modal = document.querySelector(".modal");
+let overlay = document.querySelector(".overlay");
+
+function openModal() {
+  modal.classList.add("active");
+  overlay.classList.add("overlayactive");
+}
+function closeModal() {
+  modal.classList.remove("active");
+  overlay.classList.remove("overlayactive");
+}
